442e2617f457528ac36ddd3129d5483b467bddf6
Pierre Prinetti <pierreprinetti@gmail.com>
2018-03-23 16:17:34 +0100

    show full link in doctors website

--- doctor/script.js
+++ doctor/script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const linkMsg = '<h2>You are authenticated!</h2><p>Download your data: <a href="web+phr://s3.eu-central-1.amazonaws.com/ephri/encounter.json">click here</a></p>'
+const linkMsg = '<h2>You are authenticated!</h2><p>Download your data: <a href="web+phr://s3.eu-central-1.amazonaws.com/ephri/encounter.json">web+phr://s3.eu-central-1.amazonaws.com/ephri/encounter.json</a></p>'
 
 const showLink = () => {
   document.getElementById('download').innerHTML = linkMsg
